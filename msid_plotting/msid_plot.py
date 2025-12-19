b5bcc9abc807e256a779c1cc609fef7c7ab11475
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-12-19 15:34:14 -0500

    Change html plot generator to allow custom templates.

--- msid_plotting/msid_plot.py
+++ msid_plotting/msid_plot.py
@@ -35,99 +35,10 @@ from bokeh.embed import file_html
 
 _T1998 = 883612736.816  #: Difference between Chandra and Epoch Time
 
-_JINJA_ENV = Environment(loader=PackageLoader("msid_plotting", "template"))
-_TEMPLATE = _JINJA_ENV.get_template("plot.jinja")
-
-
 @np.vectorize
 def _vecdatetime(x):
     if isinstance(x, (int, float)):
         return datetime.fromtimestamp(round(x))
-
-
-class MSIDPlot(object):
-    """
-    Class for plotting parameters of Multivariate MSID interactive plot
-
-    :NOTE: Maude stores MSID's in uppercase
-    """
-
-    #: Type Hint
-    fetch_result: dict[str, Any]
-
-    def __init__(self, msids, start, stop):
-        if isinstance(msids, list):
-            self.msids = [_.upper() for _ in msids]
-        elif isinstance(msids, str):
-            self.msids = [msids.upper()]
-        else:
-            raise Exception("MSIDPlot input must be an MSID or a list of MSIDs.")
-
-        self.start = start
-        self.stop = stop
-    
-    def _query_maude(self, msids, **kwargs) -> dict:
-
-        fetch_result = maude.get_msids(
-            msids=msids, start=self.start, stop=self.stop, **kwargs
-        )
-        return fetch_result
-    
-    def fetch_maude(self) -> None:
-        """
-        Fetch the MSID Plot telemetry from the maude server and assign the raw fetch result
-        """
-        self.fetch_result = self._query_maude(msids=self.msids)
-        
-
-    def fetch_limit(self) -> None:
-        """
-        Use the limit API to fetch and set limits for the current msid set.
-        """
-        _limits = msid_limit.query_msid_limits(self.msids)
-        self.limits = _limits
-
-
-    def _generate_frames(self) -> List[Any]:
-        frames = []
-
-        for i, msid in enumerate(self.msids):
-            p = figure(title="Test", y_axis_label=msid,x_axis_label="Date", x_axis_type = 'datetime')
-            p.scatter(x=self._datetimes[msid], y=self.fetch_result["data"][i]["values"])
-
-            p.xaxis.formatter = DatetimeTickFormatter(
-                minutes="%Y:%j:%H:%M",
-                hours="%Y:%j:%H",
-                hourmin="%Y:%j:%H:%M",
-                days="%Y:%j",
-            )
-
-            frames.append([p])
-        return frames
-        
-    def generate_plot_html(self) -> str:
-        """
-        Generate plot frames and write the contents into a python jinja template.
-        """
-        frames = self._generate_frames()
-        plot = gridplot(frames)
-        html = file_html(plot, CDN, template=_TEMPLATE)
-
-        return html
-
-    def _to_datetime(self, forcerun=False) -> None:
-        """
-        Fast numerical conversions to format cxosecs into Bokeh-plottable datetimes
-        """
-        if not hasattr(self, "_datetimes") or forcerun:
-            _datetimes = {}
-            for idx in range(
-                len(self.msids)
-            ):  #: fetch result indexed by integers from provided msid iterable
-                _msid = self.fetch_result["data"][idx]["msid"]
-                _time = self.fetch_result["data"][idx]["times"]  #: cxosecs
-                _datetimes[_msid] = _vecdatetime(ne.evaluate("_time + _T1998"))
-            self._datetimes = _datetimes
 
 class JinjaTemplateEnv(object):
     """
@@ -165,6 +76,8 @@ class JinjaTemplateEnv(object):
                 raise FileNotFoundError("Specified path is not a directory.")
             if os.path.isfile(filepath):
                 raise FileNotFoundError(f"Could not find filepath: {filepath}")
+
+JINJA_TEMPLATE_ENV = JinjaTemplateEnv()
 
 class CommCheck(object):
     """
@@ -244,3 +157,92 @@ class CommCheck(object):
 
     def __str__(self):
         return pformat(self.__dict__)
+
+class MSIDPlot(object):
+    """
+    Class for plotting parameters of Multivariate MSID interactive plot
+
+    :NOTE: Maude stores MSID's in uppercase
+    """
+
+    #: Type Hint
+    fetch_result: dict[str, Any]
+
+    def __init__(self, msids, start, stop):
+        if isinstance(msids, list):
+            self.msids = [_.upper() for _ in msids]
+        elif isinstance(msids, str):
+            self.msids = [msids.upper()]
+        else:
+            raise Exception("MSIDPlot input must be an MSID or a list of MSIDs.")
+
+        self.start = start
+        self.stop = stop
+    
+    def _query_maude(self, msids, **kwargs) -> dict:
+
+        fetch_result = maude.get_msids(
+            msids=msids, start=self.start, stop=self.stop, **kwargs
+        )
+        return fetch_result
+    
+    def fetch_maude(self) -> None:
+        """
+        Fetch the MSID Plot telemetry from the maude server and assign the raw fetch result
+        """
+        self.fetch_result = self._query_maude(msids=self.msids)
+        
+
+    def fetch_limit(self) -> None:
+        """
+        Use the limit API to fetch and set limits for the current msid set.
+        """
+        _limits = msid_limit.query_msid_limits(self.msids)
+        self.limits = _limits
+
+
+    def _generate_frames(self) -> List[Any]:
+        frames = []
+
+        for i, msid in enumerate(self.msids):
+            p = figure(title="Test", y_axis_label=msid,x_axis_label="Date", x_axis_type = 'datetime')
+            p.scatter(x=self._datetimes[msid], y=self.fetch_result["data"][i]["values"])
+
+            p.xaxis.formatter = DatetimeTickFormatter(
+                minutes="%Y:%j:%H:%M",
+                hours="%Y:%j:%H",
+                hourmin="%Y:%j:%H:%M",
+                days="%Y:%j",
+            )
+
+            frames.append([p])
+        return frames
+        
+    def generate_plot_html(self, template_name = None) -> str:
+        """
+        Generate plot frames and write the contents into a python jinja template.
+        """
+        frames = self._generate_frames()
+        plot = gridplot(frames)
+        if template_name is None: 
+            template = JINJA_TEMPLATE_ENV.env.get_template("plot.jinja")
+        else:
+            template = JINJA_TEMPLATE_ENV.env.get_template(template_name)
+
+        html = file_html(plot, CDN, template=template)
+
+        return html
+
+    def _to_datetime(self, forcerun=False) -> None:
+        """
+        Fast numerical conversions to format cxosecs into Bokeh-plottable datetimes
+        """
+        if not hasattr(self, "_datetimes") or forcerun:
+            _datetimes = {}
+            for idx in range(
+                len(self.msids)
+            ):  #: fetch result indexed by integers from provided msid iterable
+                _msid = self.fetch_result["data"][idx]["msid"]
+                _time = self.fetch_result["data"][idx]["times"]  #: cxosecs
+                _datetimes[_msid] = _vecdatetime(ne.evaluate("_time + _T1998"))
+            self._datetimes = _datetimes
